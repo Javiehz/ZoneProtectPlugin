26b84bae0d17d38ddb8942ac6953427815ba2c34
Javiehz <javier.encher@educa.jcyl.es>
2025-09-27 18:44:27 +0200

    Registro cuando un jugador construye bloque.

--- src/main/java/ZoneProtect/BlockListener.java
+++ src/main/java/ZoneProtect/BlockListener.java
@@ -4,13 +4,18 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 
 public class BlockListener implements Listener {
 
+    public void onBlockBuild(BlockPlaceEvent event){
+        Player player = event.getPlayer();
+        player.sendMessage("INFO :: HAS CONSTRUIDO UN BLOQUE.");
+    }
 
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event){
         Player player = event.getPlayer();
-        player.sendMessage("(INFO) :: HAS ROTO UN BLOQUE");
+        player.sendMessage("(INFO) :: HAS ROTO UN BLOQUE.");
     }
 }
